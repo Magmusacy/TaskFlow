48c4fad7396d206eebba2b74f37fb6b98cb0a1c3
Magmusacy <xpfk.098@gmail.com>
2023-09-13 15:06:18 +0200

    Remove private properties and some getter/setter methods

--- src/project_component/project.js
+++ src/project_component/project.js
@@ -1,18 +1,14 @@
 export default class Project {
     id;
     title;
-    #tasks = [];
+    tasks = [];
 
     constructor(id, title) {
         this.id = id;
         this.title = title;
     }
 
-    get tasks() {
-        return this.#tasks;
-    }
-
     addNewTask(task) {
-        this.#tasks.push(task);
+        this.tasks.push(task);
     }
 }
--- src/task_component/task.js
+++ src/task_component/task.js
@@ -2,48 +2,36 @@ export default class Task {
     id;
     title;
     description;
-    #dueDate;
-    #priority;
-    #isCompleted = false;
+    dueDate;
+    priority;
+    isCompleted;
     
-    constructor(id, title, description, dueDate, priority) {
+    constructor(id, title, description, dueDate, priority, isCompleted = false) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.isCompleted = isCompleted;
     }
 
     set dueDate(value) {
-        console.log(value === Date.now(), Date.now());
         if (value > Date.now()) {
-            this.#dueDate = value;
+            this.dueDate = value;
         } else {
             throw new Error('Due date must be set to a future date.')
         }
     }
 
-    get dueDate() {
-        return this.#dueDate;
-    }
-
     set priority(value) {
         if (value < 5 && value > 0) {
-            this.#priority = value;
+            this.priority = value;
         } else {
             throw new Error('Priority invalid, should be between 1 and 4');
         } 
     }
 
-    get priority() {
-        return this.#priority;
-    }
-
-    get isCompleted() {
-        return this.#isCompleted;
-    }
-
     changeCompletionStatus() {
-        this.#isCompleted = this.#isCompleted ? false : true; 
+        this.isCompleted = this.isCompleted ? false : true; 
     }
 }
